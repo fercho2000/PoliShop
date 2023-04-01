2312b2ddb6ca75a22770a782f5e3c4c64c1eef62
Jose Fernando U <44246337+fercho2000@users.noreply.github.com>
2023-04-01 14:03:46 -0500

    GRAL: Se agregan componentes y estructura para el manejo de navegacion y componente base de login con estructura para su lógica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './shared/home/home.component';
+import { LoginModule } from './feature/login/login.module';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    LoginModule,
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
