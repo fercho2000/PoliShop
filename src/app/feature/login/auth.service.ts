b489aaf611fd4519bf2ee72c99fc14ef67a7372c
Jose Fernando U <44246337+fercho2000@users.noreply.github.com>
2023-04-12 22:11:21 -0500

    GRAL: Actualizacion base

--- src/app/feature/login/auth.service.ts
+++ src/app/feature/login/auth.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  // Portal del cliente
+// Nombre clave	Nombre	Clave API	Creado	
+// api-poli-shop	JOSE FERNANDO USUGA FLOREZ	aKh055W4PjWR8ek8ovaSkd	04/11/2023 21:22
+  getUser() {
+    const apiKey = 'aKh055W4PjWR8ek8ovaSkd';
+    const email = 'jose@gmail.com';
+    const pass='am9zZTEyMw=='; 
+    const url = `https://quintadb.com/apps.json?rest_api_key=aKh055W4PjWR8ek8ovaSkd&_filters={"email":"${email}","password":"${pass}"}`;
+    return this.http.get(url);
+  }
 }
--- src/app/feature/login/login/login.component.ts
+++ src/app/feature/login/login/login.component.ts
@@ -1,21 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-
+import { AuthService } from '../auth.service';
+import { log } from 'console';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private authServise: AuthService
+  ) {}
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required]
+      password: ['', Validators.required],
     });
   }
 
@@ -23,7 +27,13 @@ export class LoginComponent implements OnInit {
     if (this.loginForm.valid) {
       console.log(this.loginForm.value);
       // Aquí puedes enviar los datos del formulario a un servidor o hacer otras acciones.
+      this.authServise.getUser().subscribe((data) => {
+        console.log('data que llega ', data);
+      });
+
+      // Portal del cliente
+      // Nombre clave	Nombre	Clave API	Creado
+      // api-poli-shop	JOSE FERNANDO USUGA FLOREZ	aKh055W4PjWR8ek8ovaSkd	04/11/2023 21:22
     }
   }
-
 }
