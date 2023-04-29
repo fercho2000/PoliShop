1932297479ff57816e7d8448b558d5c288ef6e86
rmira <84809039+rmira03@users.noreply.github.com>
2023-04-28 23:41:56 -0500

    Update módulos de "admin"
    Se actualizan los módulos de "admin" con localStorage,. Se actualizan las alertas con botón para eliminarlas.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeContentComponent } from './pages/home/home-content.component';
 import { DetalleProductoComponent } from './pages/detalle-producto/detalle-producto.component';
+import { ClientesComponent } from './pages/admin/clientes/clientes.component';
+import { ProveedoresComponent } from './pages/admin/proveedores/proveedores.component';
+import { ProductosComponent } from './pages/admin/productos/productos.component';
 
 
 const routes: Routes = [
@@ -10,7 +13,10 @@ const routes: Routes = [
 
   { path: 'home', component: HomeContentComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'detalle-producto', component: DetalleProductoComponent }
+  { path: 'detalle-producto', component: DetalleProductoComponent },
+  { path: 'clientes', component: ClientesComponent },
+  { path: 'productos', component: ProductosComponent },
+  { path: 'proveedores', component: ProveedoresComponent }
 
 ];
 
---
+++ src/app/pages/admin/proveedores/proveedores.ts
@@ -0,0 +1,7 @@
+// Creamos una interfaz para definir la estructura del objeto que utilizaremos
+
+export interface Providers {
+    name: String;
+    category: String;
+    email: String;
+}
