43ce2fe70c3324f28bf3799468e2f5382123e49c
Ivan H CArvajal Acevedo <ivanchoh99@gmail.com>
2023-04-29 00:00:39 -0500

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { LoginComponent } from './pages/login/login.component';
 import { HomeContentComponent } from './pages/home/home-content.component';
 import { DetalleProductoComponent } from './pages/detalle-producto/detalle-producto.component';
 import { ListaProductosComponent } from './pages/lista-productos/lista-productos.component';
+import { ClientesComponent } from './pages/admin/clientes/clientes.component';
+import { ProveedoresComponent } from './pages/admin/proveedores/proveedores.component';
+import { ProductosComponent } from './pages/admin/productos/productos.component';
 
 
 const routes: Routes = [
@@ -12,7 +15,11 @@ const routes: Routes = [
   { path: 'home', component: HomeContentComponent },
   { path: 'login', component: LoginComponent },
   { path: 'detalle-producto', component: DetalleProductoComponent },
-  { path: 'lista/:categoria', component: ListaProductosComponent }
+  { path: 'lista/:categoria', component: ListaProductosComponent },
+  { path: 'clientes', component: ClientesComponent },
+  { path: 'proveedores', component: ProveedoresComponent },
+  { path: 'productos', component: ProductosComponent },
+
 
 ];
 
